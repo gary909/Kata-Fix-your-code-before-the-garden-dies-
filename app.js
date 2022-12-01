25c3006eb811c285fb35fd89f3eb82b571385d7a
gary909 <garywhite61@hotmail.com>
2022-12-01 13:40:04 +0100

    code working

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 function rainAmount(mm) {
-    if (rainAmount = 40) {
-        return "You need to give your plant " + { rainAmount - 40
-    } + " mm of water"
-};
-if else {
-    return "Your plant has had more than enough water for today!"
-};
+    if (mm < 40) {
+        return "You need to give your plant " + (40 - mm) + "mm of water";
+    } else {
+        return "Your plant has had more than enough water for today!"
+    };
 }
 
 console.log(rainAmount(100)); // "Your plant has had more than enough water for today!"
